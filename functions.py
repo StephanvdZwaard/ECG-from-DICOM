a71171113661ce7cead95610103d15b5cb74c226
StephanvdZwaard <stephan_vanderzwaard@live.nl>
2024-05-06 17:23:42 +0200

    Toevoegen functie voor prefix voor batches

--- functions.py
+++ functions.py
@@ -17,3 +17,20 @@ def recursive_copy(path):
         else:
 
             recursive_copy(file)
+
+def get_batch_prefix(batch):
+            
+    if batch < 0 or type(batch) is not int:
+        raise ValueError('Incorrect batch index: inspect selected start/end index')
+    elif batch < 100:
+        prefix = '0000'
+    elif batch <1000:
+        prefix = '000'
+    elif batch <10000:
+        prefix = '00'
+    elif batch <100000:
+        prefix = '0'
+    else:
+        prefix = ''
+
+    return(prefix)
